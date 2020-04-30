ac22ef42656d1a9dace055055d8284267ec896b7
Small peng <xz87060361@126.com>
2020-04-30 18:00:26 +0800

    增加gitignore

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+// index.test.js
+var expect = require('chai').expect;
+var starWars = require('./index.js');
+
+describe('starwars-names', function() {
+    describe('all', function () {
+        it('should be an array of strings', function () {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                })
+            }
+        });
+    });
+
+    describe('random', function () {
+        it('should return a random item from the starWars.all', function () {
+            var randomItem = starWars.random();
+            expect(starWars.all).to.include(randomItem);
+        });
+    });
+});
