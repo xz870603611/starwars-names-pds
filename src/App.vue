3ac94d8d28adca29ae4304c6b5a1339c4cd45b9e
pengdashuai <xz87060361@126.com>
2020-05-09 16:40:23 +0800

    创建vue 测试项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+<script>
+    import {StringTool} from 'pdstoolsdk';
+    export default {
+        name: 'App',
+        components: {},
+        mounted() {
+            console.log(StringTool.isMobile('15295159905'))
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
